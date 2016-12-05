861860cc92ac29e07cff7701653a68b970ae61e8
MMWeb87 <marc@melliger.com>
2016-12-05 17:34:29 +0100

    Container for all charged vehicles

---
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/stations/ChargingVehicles.java
@@ -0,0 +1,29 @@
+package org.matsim.contrib.carsharing.stations;
+
+import java.util.HashSet;
+
+import org.matsim.contrib.carsharing.vehicles.StationBasedBEV;
+
+/**
+ * This class with static methods stores all the charged vehicles.
+ * @author marc
+ * 
+ */
+public class ChargingVehicles {
+	
+	private static HashSet<StationBasedBEV> chargingVehicles;
+	
+	public static void addToChargingVehicles(StationBasedBEV vehicle){
+		chargingVehicles.add(vehicle);
+	}
+	
+	public static void removeFromChargingVehicles(StationBasedBEV vehicle){
+		chargingVehicles.remove(vehicle);
+	}
+	
+	public static HashSet<StationBasedBEV> getChargingVehicles(){
+		return chargingVehicles;
+	}
+	
+
+}
