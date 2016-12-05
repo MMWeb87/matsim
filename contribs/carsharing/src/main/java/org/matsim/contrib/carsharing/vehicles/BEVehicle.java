4fe43ce047641b452ea57074505ae0ae1ed611a6
MMWeb87 <marc@melliger.com>
2016-12-05 18:31:32 +0100

    charge & uncharge / fully charged / attach & remove Charger

--- contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/vehicles/BEVehicle.java
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/vehicles/BEVehicle.java
@@ -1,11 +1,16 @@
 package org.matsim.contrib.carsharing.vehicles;
 
-public interface BEVehicle {
+public interface BEVehicle{
 	
-	public int getChargingLevel(); //kWh
-	public void setChargingLevel(int chargingLevel); //kWh
+	public double getChargingLevel(); //kWh
+	public void setChargingLevel(double chargingLevel); //kWh
 
-	public int getBatteryCapacity(); // kWh
-	public int getEnergyConsumption(); // kWh/km
+	public double getBatteryCapacity(); // kWh
+	public double getEnergyConsumption(); // kWh/km
+	
+	public boolean isFullyCharged();
+	public void charge(double charge);
+	public void uncharge(double charge);
+
 
 }
