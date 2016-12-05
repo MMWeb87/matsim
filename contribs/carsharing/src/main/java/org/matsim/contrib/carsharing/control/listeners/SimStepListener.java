75a9fc32826e8c15d04f9f367a405d9dedf0c189
MMWeb87 <marc@melliger.com>
2016-12-05 18:33:34 +0100

    New Listener to charge at every TimeStep

---
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/control/listeners/SimStepListener.java
@@ -0,0 +1,33 @@
+package org.matsim.contrib.carsharing.control.listeners;
+
+import java.util.HashSet;
+
+import org.matsim.contrib.carsharing.stations.ChargingVehicles;
+import org.matsim.contrib.carsharing.vehicles.StationBasedBEV;
+import org.matsim.core.mobsim.framework.events.MobsimAfterSimStepEvent;
+import org.matsim.core.mobsim.framework.listeners.MobsimAfterSimStepListener;
+
+
+
+public class SimStepListener implements MobsimAfterSimStepListener {
+
+	public SimStepListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void notifyMobsimAfterSimStep(MobsimAfterSimStepEvent e) {
+		
+		
+		// get all the vehicles that are currently being charged and charge em:
+		
+		HashSet<StationBasedBEV> vehicles = ChargingVehicles.getChargingVehicles();
+		
+		for(StationBasedBEV vehicle: vehicles){
+			vehicle.getAttachedCharger().chargeVehicle(vehicle);
+		}
+
+		
+	}
+
+}
